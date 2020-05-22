731a06053db216f8918f24f908c057ec7292ae20
dnns.hrrn <dennis.herren@students.fhnw.ch>
2020-05-22 09:50:39 +0200

    changes

--- StringVision/StringVision_dennis.py
+++ StringVision/StringVision_dennis.py
@@ -18,29 +18,7 @@ ret, binary = cv2.threshold(gray,100,255,cv2.THRESH_BINARY_INV)
 eroded = cv2.erode(binary, None, iterations=1)
 
 
-def fill_holes(img):
-    im_th = img.copy()
 
-    # Copy the thresholded image.
-    im_floodfill = im_th.copy()
-
-    # Mask used to flood filling.
-    # Notice the size needs to be 2 pixels than the image.
-    h, w = im_th.shape[:2]
-    mask = np.zeros((h+2, w+2), np.uint8)
-
-    # Floodfill from point (0, 0)
-    if img[0,0] != 0:
-        print("WARNING: Filling something you shouldn't")
-    cv2.floodFill(im_floodfill, mask, (0,0), 255);
-
-    # Invert floodfilled image
-    im_floodfill_inv = cv2.bitwise_not(im_floodfill)
-
-    # Combine the two images to get the foreground.
-    im_out = im_th | im_floodfill_inv
-
-    return im_out
 
 
 flooded_inv = fill_holes(eroded)
