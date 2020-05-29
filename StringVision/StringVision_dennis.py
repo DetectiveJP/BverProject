10734d547c66ba69888bfd4e8ef4c9edf628d412
Dario Spadola <dario.95@hispeed.ch>
2020-05-29 08:58:24 +0200

    Merge dario.py with dennis.py

--- StringVision/StringVision_dennis.py
+++ StringVision/StringVision_dennis.py
@@ -5,6 +5,7 @@ import cv2
 from helper.image_prepartion import extract_cell, find_conturs, undistort, calibrate
 from helper.image_loader import load_images, load_image_list
 from helper.measurment_detector import get_center, draw_center, get_width
+from helper.fragment_detector import find_fragment
 
 # Vertical = x, horizontal = y
 x_scale_factor = 0.166313559322033
@@ -27,6 +28,7 @@ for filename in list_image:
     img_undist = undistort(img_orig, mtx, dist, 0)
     cv2.imshow("undistorted", img_undist)
     img_cell = extract_cell(img_undist)
+    cnts, img_fragment = find_fragment(img_cell)
 
     cell_contoure, img_show_contoures = find_conturs(img_cell)
     cell_center_x, cell_center_y, cell_angle = get_center(cell_contoure)
@@ -41,6 +43,8 @@ for filename in list_image:
 
     print("Cell orientation [deg]: " + str(np.rad2deg(cell_angle)) + "\n")
 
+    print("Counted Fragments: " + str(cnts))
+
     binary_save_path = os.path.join(image_src_path, 'Binary')
     if os.path.exists(binary_save_path):
         cv2.imwrite(os.path.join(binary_save_path, filename[:-4] + '_binary.bmp'), img_cell)
@@ -48,6 +52,10 @@ for filename in list_image:
     result_save_path = os.path.join(image_src_path, 'Result')
     if os.path.exists(result_save_path):
         cv2.imwrite(os.path.join(result_save_path, filename[:-4] + '_result.bmp'), img_show_center)
+
+    fragment_save_path = os.path.join(image_src_path, 'Fragment')
+    if os.path.exists(fragment_save_path):
+        cv2.imwrite(os.path.join(fragment_save_path, filename[:-4] + '_fragment.bmp'), img_fragment)
 
 cv2.waitKey()
 cv2.destroyAllWindows()
